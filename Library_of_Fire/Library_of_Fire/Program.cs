a1aa9e4b43f6e09fd51d0247451a1acaddc11279
actonlauren <actonlauren@yahoo.com>
2023-11-29 13:08:33 -0500

    Initial Commit

---
+++ Library_of_Fire/Library_of_Fire/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Library_of_Fire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
